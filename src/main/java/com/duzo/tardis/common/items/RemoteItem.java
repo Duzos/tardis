5590c15e669ec6593a248636e0ecbf3dc23665dc
Duzo <james.hall01@outlook.com>
2023-04-23 19:44:04 +0100

    Fixed a bug with hover text by removing hover text :sunglasses:

--- src/main/java/com/duzo/tardis/common/items/RemoteItem.java
+++ src/main/java/com/duzo/tardis/common/items/RemoteItem.java
@@ -1,6 +1,7 @@
 package com.duzo.tardis.common.items;
 
 import com.duzo.tardis.core.util.AbsoluteBlockPos;
+import com.duzo.tardis.tardis.TARDIS;
 import com.duzo.tardis.tardis.blocks.entities.ExteriorBlockEntity;
 import com.duzo.tardis.tardis.links.LinkableItem;
 import com.duzo.tardis.tardis.io.TARDISTravel;
@@ -10,6 +11,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
@@ -20,22 +22,30 @@ import java.util.List;
 
 import static com.duzo.tardis.tardis.io.TARDISTravel.searchForNearestAirBlock;
 
-public class RemoteItem extends LinkableItem {
+public class RemoteItem extends Item {
+    private TARDIS tardis;
     public RemoteItem(Properties properties) {
         super(properties);
     }
 
-    @Override
-    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
-        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
-
-        RemoteItem item = (RemoteItem) pStack.getItem();
-        if (item.getTARDIS() != null) {
-            pTooltip.add(Component.translatable("Linked TARDIS: " + item.getTARDIS().getUuid()).withStyle(ChatFormatting.GREEN));
-        } else {
-            pTooltip.add(Component.translatable("No linked TARDIS").withStyle(ChatFormatting.RED));
-        }
+    private void link(TARDIS tardis) {
+        this.tardis = tardis;
     }
+
+    protected TARDIS getTARDIS() {
+        return this.tardis;
+    }
+
+//    @Override
+//    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+//        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
+//
+//        if (this.getTARDIS() != null) {
+//            pTooltip.add(Component.translatable("Linked TARDIS: " + this.getTARDIS().getUuid()).withStyle(ChatFormatting.GREEN));
+//        } else {
+//            pTooltip.add(Component.translatable("No linked TARDIS").withStyle(ChatFormatting.RED));
+//        }
+//    }
 
     @Override
     public InteractionResult useOn(UseOnContext context) {
