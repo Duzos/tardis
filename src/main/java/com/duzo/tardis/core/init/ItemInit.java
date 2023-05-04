1757a96fac6da5b977476c68c268277f9df921d6
Duzo <james.hall01@outlook.com>
2023-05-04 18:12:37 +0100

    Added copper and toyota items

--- src/main/java/com/duzo/tardis/core/init/ItemInit.java
+++ src/main/java/com/duzo/tardis/core/init/ItemInit.java
@@ -19,6 +19,8 @@ public class ItemInit {
     public static final RegistryObject<TARDISSpawnItem> THIRD_TARDIS_ITEM = ITEMS.register("third_tardis_item", () -> new TARDISSpawnItemBuilder(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)).interior("eighth").exterior("third"));
     public static final RegistryObject<TARDISSpawnItem> BAKER_TARDIS_ITEM = ITEMS.register("baker_tardis_item", () -> new TARDISSpawnItemBuilder(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)).interior("baker").exterior("baker"));
     public static final RegistryObject<TARDISSpawnItem> SECOND_TARDIS_ITEM = ITEMS.register("second_tardis_item", () -> new TARDISSpawnItemBuilder(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)).interior("baker").exterior("second"));
+    public static final RegistryObject<TARDISSpawnItem> TOYOTA_TARDIS_ITEM = ITEMS.register("toyota_tardis_item", () -> new TARDISSpawnItemBuilder(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)).exterior("toyota"));
+    public static final RegistryObject<TARDISSpawnItem> COPPER_TARDIS_ITEM = ITEMS.register("copper_tardis_item", () -> new TARDISSpawnItemBuilder(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)).exterior("copper"));
 
 
     public static final RegistryObject<RemoteItem> REMOTE_ITEM = ITEMS.register("remote_item", () -> new RemoteItem(new Item.Properties().tab(CreativeTabInit.TARDIS_TAB).stacksTo(1)));
